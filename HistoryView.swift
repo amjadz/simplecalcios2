fa32a0eb9f30b013cc7e3bf5eee787efed5dcfa9
Zubair Amjad <amjadz@uw.edu>
2018-10-21 02:04:37 -0700

    added history button, and history panel

---
+++ HistoryView.swift
@@ -0,0 +1,30 @@
+//
+//  HistoryView.swift
+//  simple-calc-ios
+//
+//  Created by Zubair Amjad on 10/21/18.
+//  Copyright © 2018 Zubair Amjad. All rights reserved.
+//
+
+import UIKit
+
+class HistoryView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
